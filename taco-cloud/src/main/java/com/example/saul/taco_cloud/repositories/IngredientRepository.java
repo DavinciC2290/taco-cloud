1f775472617b88fa6ae3095bae362a2203c26780
sauldavid <sauldavincic2290@gmail.com>
2025-09-03 10:21:28 -0600

    taco-cloud with register, login, authentication and authorization with Spring Boot Security

--- taco-cloud/src/main/java/com/example/saul/taco_cloud/repositories/IngredientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.saul.taco_cloud.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.saul.taco_cloud.domain.Ingredient;
-
-public interface IngredientRepository extends JpaRepository<Ingredient, String> {
-    
-}
--- taco-cloud/src/main/java/com/example/saul/taco_cloud/repositories/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.saul.taco_cloud.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.saul.taco_cloud.domain.Taco;
-
-public interface TacoRepository extends JpaRepository <Taco, Long>{
-    
-}
